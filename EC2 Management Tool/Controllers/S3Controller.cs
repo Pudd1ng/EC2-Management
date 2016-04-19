08b0bf219391c119057bdf2ec91c46ca3b1af88a
Conner Bell <Conner Bell>
2016-04-19 23:44:08 +0100

    ebs volumes almost finished. Need to get cascade lists working for detaching

--- EC2 Management Tool/Controllers/S3Controller.cs
+++ EC2 Management Tool/Controllers/S3Controller.cs
@@ -15,9 +15,38 @@ namespace EC2_Management_Tool.Controllers
         {
             dynamic model = new ExpandoObject();
             S3Model s3m = new S3Model();
+            EC2ServersModels ec2m = new EC2ServersModels();
             var s3vols = s3m.getVolumes();
+            var freevols = s3m.getFreeVolumes();
+            var instances = ec2m.getInstances();
             model.volumes = s3vols;
+            model.freevolumes = freevols;
+            model.instances = instances;
             return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult CreateVolume(int size, string type, string zone)
+        {
+            S3Model s3m = new S3Model();
+            s3m.CreateVolume(size, type, zone);
+            return RedirectToAction("S3");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteVolume(string volumeId)
+        {
+            S3Model s3m = new S3Model();
+            s3m.DeleteVolume(volumeId);
+            return RedirectToAction("S3");
+        }
+
+        [HttpPost]
+        public ActionResult AttachVolume(string volumeId, string instanceId, string device)
+        {
+            S3Model s3m = new S3Model();
+            s3m.AttachVolume(volumeId, instanceId, device);
+            return RedirectToAction("S3");
         }
     }
 }
--- EC2 Management Tool/Models/S3Model.cs
+++ EC2 Management Tool/Models/S3Model.cs
@@ -17,5 +17,68 @@ namespace EC2_Management_Tool.Models
             var allS3Response = client.DescribeVolumes(allS3Reqeust);
             return allS3Response;
         }
+
+        public object getFreeVolumes()
+        {
+            List<string> freeIds = new List<string>();
+            var allS3Reqeust = new DescribeVolumesRequest();
+            var allS3Response = client.DescribeVolumes(allS3Reqeust);
+            foreach(var volume in allS3Response.Volumes)
+            {
+                if (volume.Attachments.Count == 0 )
+                {
+                    freeIds.Add(volume.VolumeId);
+                }
+            }
+            return freeIds;
+        }
+
+        public void DeleteVolume(string volumeId)
+        {
+            var s3DeleteRequest = new DeleteVolumeRequest();
+            s3DeleteRequest.VolumeId = volumeId;
+            var s3DeleteResponse = client.DeleteVolume(s3DeleteRequest);
+        }
+
+        public void CreateVolume(int size, string type, string zone)
+        {
+            var s3CreateRequest = new CreateVolumeRequest()
+            {
+               AvailabilityZone = zone,
+               Size = size,
+               VolumeType = type,
+            };
+            var s3CreateResponse = client.CreateVolume(s3CreateRequest);
+        }
+
+        public object AttachVolume(string volumeId, string instanceId, string device)
+        {
+            try {
+                var attachRequest = new AttachVolumeRequest()
+                {
+                    InstanceId = instanceId,
+                    VolumeId = volumeId,
+                    Device = device
+                };
+                var attachResponse = client.AttachVolume(attachRequest);
+                return 0;
+            }
+            catch(Exception ex)
+            {
+                return ex;
+            }
+        }
+
+        public void DetachVolume(string volumeId, string instanceId, string device)
+        {
+            var DetachRequest = new DetachVolumeRequest()
+            {
+                InstanceId = instanceId,
+                VolumeId = volumeId,
+                Device = device
+            };
+
+            var DeleteResponse = client.DetachVolume(DetachRequest);
+        }
     }
 }
