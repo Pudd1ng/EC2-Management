b49e23ebd36a59c24539a9c0dd385e2482bddb48
Conner Bell <connerbell@outlook.com>
2016-05-14 05:46:43 +0100

    Added SQL Queries and Scheduling, Need to fix WebMatrix incompatibility

--- EC2 Management Tool/Controllers/ScheduleController.cs
+++ EC2 Management Tool/Controllers/ScheduleController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EC2_Management_Tool.Models;
+using System.Dynamic;
 
 namespace EC2_Management_Tool.Controllers
 {
     public class ScheduleController : Controller
     {
         // GET: Schedule
-        public ActionResult Index()
+        public ActionResult Schedule()
         {
-            return View();
+            dynamic model = new ExpandoObject();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult CreateSchedule(string Name, string Days, string Hours)
+        {
+            ScheduleModel sm = new ScheduleModel();
+            sm.createSchedule(Name, Days, Hours);
+            return RedirectToAction("Schedule");
         }
     }
 }
--- EC2 Management Tool/Models/ScheduleModel.cs
+++ EC2 Management Tool/Models/ScheduleModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,47 @@ namespace EC2_Management_Tool.Models
 {
     public class ScheduleModel
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Days { get; set; }
+        public string Hours { get; set; }
+
+        /*public ScheduleModel(int id, string name, string days, string hours)
+        {
+            ID = id;
+            Name = name;
+            Days = days;
+            Hours = hours;
+        }*/
+
+        public void createSchedule(string Name, string Days, string Hours)
+        {
+            string connectionString = "Data Source=.;Initial Catalog=EC2ManagementTool;Integrated Security=True";
+            var queryString = string.Format("INSERT INTO Schedule (Name, Days, Hours) VALUES ('{0}' , '{1}', '{2}')",Name, Days, Hours);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(queryString, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }       
+        }
+        //Maybe pass in queries and form one method here? conform to DRY
+        public void deleteSchedule(string Name)
+        {
+            string connectionString = "Data Source=.;Initial Catalog=EC2ManagementTool;Integrated Security=True";
+            var queryString = string.Format("DELETE FROM Schedule WHERE Name = '{0}'", Name);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(queryString, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
     }
 }
